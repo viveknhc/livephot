26692c234c65b5f8f32ecd12fa6fef863b903346
viveknhc <viveknhc@gmail.com>
2022-09-02 15:24:13 +0530

    to database

--- web/views.py
+++ web/views.py
@@ -5,10 +5,12 @@ from django.core.files.base import ContentFile
 from django.core.files.temp import NamedTemporaryFile
 from django.shortcuts import redirect, render
 from .models import *
+import base64
 
 def index(request):
     print(request.POST)
     if request.method == 'POST':
-        image = request.POST['imgurl']
-        
+        image = request.FILES['imgurl']
+        base64_message = image.decode('ascii')
+        Image.create(image=base64_message)  
     return render(request,"index.html")
